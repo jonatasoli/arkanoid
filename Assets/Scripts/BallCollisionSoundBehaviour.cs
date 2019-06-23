ae932355131d14dfddd97f613d520a7d2bd2c32b
Jônatas Oliveira <contato@jonatasoliveira.me>
2019-06-23 06:07:26 -0300

    add sound fx

---
+++ Assets/Scripts/BallCollisionSoundBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCollisionSoundBehaviour : MonoBehaviour
+{
+    public AudioSource soundFXWall;
+
+    public AudioSource soundFXBrickHit;
+
+    public AudioSource souundFXBrickBroken;
+
+    public AudioSource soundTrack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundTrack.Play();
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Wall" || collision.collider.tag == "Paddle")
+            soundFXWall.Play();
+        else if (collision.collider.tag == "Brick")
+            if (collision.collider.GetComponent<BrickCrackBehaviour>().getLife < 1)
+                soundFXBrickHit.Play();
+            else
+                souundFXBrickBroken.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
